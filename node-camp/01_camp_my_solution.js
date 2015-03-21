03b9da96673001815185bbf59e2a6dfd5f7fcc1a
Ptolemy <ptolemybarnes@gmail.com>
2015-03-21 15:01:12 +0000

    Deleted a bunch of trivial stuff

--- node-camp/01_camp_my_solution.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs')
- 
-var source      = fs.createReadStream('sonnet.txt');
-var destination = fs.createWriteStream('destination.txt');
-var secondDestination = fs.createWriteStream('secondDestination.txt')
- 
-source.pipe(destination).pipe(secondDestination);
